3d25c75395651da53fab82d5e9be25376982d5de
akejolin <akejolin@gmail.com>
2020-09-04 01:19:20 +0200

    added jest and a test

---
+++ src/__mock__/App.js
@@ -0,0 +1,7 @@
+import React from '../web_modules/react.js';
+
+function App({}) {
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("div", "Hello world!"))
+}
+
+export default App;
---
+++ src/locate-imports.test.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const locateImports = require('./locate-imports')
+
+describe('locate-imports', () => {
+  it('should find some imports in file', () => {
+    const fileList = [
+      `${path.join(__dirname, '__mock__/index.js')}`,
+    ]
+    const result = locateImports(fileList)
+    expect(result[0].imports).toEqual([
+        "../__snowpack__/env.js",
+        "../web_modules/react.js",
+        "../web_modules/react-dom.js",
+        "./App.js",
+    ])
+  })
+
+  it('should not dig in folders or non-existing files', () => {
+    const fileList = [
+      `${path.join(__dirname, '__mock__')}`,
+      `${path.join(__dirname, '__mock__/i-do-not-exist.js')}`,
+    ]
+    const result = locateImports(fileList)
+    expect(result).toEqual([])
+  })
+
+})
